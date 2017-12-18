c05c15fc6a4c278b2a5ebdcd36f9c29867d78047
牛贵盛 <niuguisheng@niuguishengdemacbook-pro.local>
2017-12-18 20:38:46 +0800

    Initial Commit

---
+++ helloobjcworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloobjcworld
+//
+//  Created by 牛贵盛 on 2017/12/18.
+//  Copyright © 2017年 thcompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloobjcworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloobjcworld
+//
+//  Created by 牛贵盛 on 2017/12/18.
+//  Copyright © 2017年 thcompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
